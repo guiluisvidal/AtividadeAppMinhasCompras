692094944cc7ec88ee8ed39e6c85d59157b18c7a
guiluisvidal <guilherme.souza478@etec.sp.gov.br>
2025-08-24 16:54:45 -0300

    Atualização

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,27 @@
-﻿namespace AtividadeAppMinhasCompras
+﻿using AtividadeAppMinhasCompras.Helpers;
+
+namespace AtividadeAppMinhasCompras
 {
     public partial class App : Application
     {
+        static SQLiteDataBaseHelper _db;
+
+        public static SQLiteDataBaseHelper Db
+        {
+            get
+            {
+                if (_db == null)
+                {
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), 
+                        "db_sqlite_minhas_compras.db3");
+
+                    _db = new SQLiteDataBaseHelper (path);
+                }
+                return _db;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
@@ -10,4 +30,4 @@
             MainPage = new NavigationPage(new Views.ListaProduto());
         }
     }
-}
+} 
--- Views/NovoProduto.xaml.cs
+++ Views/NovoProduto.xaml.cs
@@ -1,3 +1,6 @@
+using AtividadeAppMinhasCompras.Models;
+using System.Threading.Tasks;
+
 namespace AtividadeAppMinhasCompras.Views;
 
 public partial class NovoProduto : ContentPage
@@ -6,4 +9,24 @@ public partial class NovoProduto : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+			Produto p = new Produto
+			{
+				Descricao = txt_descricao.Text,
+				Quantidade = Convert.ToDouble(txt_Quantidade.Text),
+				Preco = Convert.ToDouble(txt_preco.Text)
+			};
+			await App.Db.InsertProduto(p);
+			await DisplayAlert("Sucesso!", "Registro inserido", "OK");
+
+		}
+		catch (Exception ex) 
+		{
+			await DisplayAlert("Ops", ex.Message, "OK");
+		}
+    }
 }
